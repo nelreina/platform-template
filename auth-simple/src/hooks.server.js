6057f9878bb49b89715ddc0d6b021937ae2b0fb7
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2023-11-27 10:02:54 -0400

    main: Added pocketbase client side

--- auth-simple/src/hooks.server.js
+++ auth-simple/src/hooks.server.js
@@ -1,11 +1,13 @@
 /** @type {import('./$types').ParamMatcher */
 import { redirect } from '@sveltejs/kit';
 import { base } from '$app/paths';
+import { createInstance } from '$lib/pocketbase.js';
 
 import { getSessionUser } from './lib/server/sessions';
 import { checkOtp } from './lib/server/otp';
 
 export const handle = async ({ event, resolve }) => {
+	const pb = createInstance();
 	const session = await getSessionUser(event.cookies);
 	const { user, token } = session || {};
 	event.locals.user = user;
