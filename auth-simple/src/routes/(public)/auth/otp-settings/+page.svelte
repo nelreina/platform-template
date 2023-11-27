d7189b414dedb8fa1e1619b17f0616bd9d327eb2
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2023-11-27 07:39:30 -0400

    main: Added appstore links for authenticator

--- auth-simple/src/routes/(public)/auth/otp-settings/+page.svelte
+++ auth-simple/src/routes/(public)/auth/otp-settings/+page.svelte
@@ -2,21 +2,27 @@
 	export let data;
 	export let form;
 	import { enhance } from '$app/forms';
+	import { i18n } from '$lib/stores/i18next.js';
+	import AuthenticatorAppLinks from './AuthenticatorAppLinks.svelte';
 
 	const browserSessionToken = data.browserSessionToken;
 </script>
 
 <div class="card p-10 variant-glass-tertiary">
 	<section class="flex flex-col items-center justify-center gap-5">
-		<h3 class="h3">Please Setup 2nd Factor Authentication</h3>
+		<h3 class="h3">
+			{$i18n.t('Please Setup 2nd Factor Authentication')}
+		</h3>
 
-		<p>Download Authenticator app from App Stores and scan this qrcode with your phone</p>
-
+		<p>
+			{$i18n.t('Download Authenticator app from App Stores and scan this qrcode with your phone')}
+		</p>
+		<AuthenticatorAppLinks />
 		<img src={data.qrcode} alt="qrcode" />
 		{#if form?.error}
 			<div class="alert variant-filled-error rounded-md">
 				<div class="alert-message">
-					{form?.error}
+					{$i18n.t(form?.error)}
 				</div>
 			</div>
 		{/if}
---
+++ auth-simple/src/routes/(public)/auth/otp-settings/AuthenticatorAppLinks.svelte
@@ -0,0 +1,29 @@
+<script>
+	// export let style = "";
+</script>
+
+<main class="flex items-center justify-center">
+	<div class="text-center">
+		<div class="flex space-x-4">
+			<!-- Microsoft Authenticator on Google Play -->
+			<a
+				href="https://play.google.com/store/apps/details?id=com.azure.authenticator"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="inline-block"
+			>
+				<img src="google-play.png" alt="Get it on Google Play" class="h-20" />
+			</a>
+
+			<!-- Microsoft Authenticator on the App Store -->
+			<a
+				href="https://apps.apple.com/us/app/microsoft-authenticator/id983156458"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="inline-block"
+			>
+				<img src="apple-app-store.png" alt="Download on the App Store" />
+			</a>
+		</div>
+	</div>
+</main>
