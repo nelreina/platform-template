888a877326c290cf8536f8511a28d3787bc3e825
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2023-12-20 06:17:08 -0400

    feature/mobile-drawer: enabled mobile drawer and switch light/dark mode

---
+++ auth-simple/src/lib/components/AppTitle.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { base } from '$app/paths';
+	import { PUBLIC_APP_TITLE } from '$env/static/public';
+
+	//  export let data;
+</script>
+
+<a href={base} class="flex items-center gap-3">
+	<img src="{base}/logo.png" alt="" class="w-12 h-12 rounded" />
+
+	<strong class="text-xl uppercase">{PUBLIC_APP_TITLE}</strong>
+</a>
