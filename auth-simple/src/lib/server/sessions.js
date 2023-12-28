5e9e9327347d14d40976dbd5e44834cfa1043e09
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2023-12-27 21:41:51 -0400

    main: docker node prod version 18

--- auth-simple/src/lib/server/sessions.js
+++ auth-simple/src/lib/server/sessions.js
@@ -7,69 +7,69 @@ import { base } from '$app/paths';
 const STREAM = process.env['STREAM'];
 const SERVICE = process.env['SERVICE_NAME'] || 'unknown';
 
-const SESSION_EXPIRED = process.env['SESSION_EXPIRED'];
+const SESSION_EXPIRED = process.env['SESSION_EXPIRED'] || 604800;
 const cookieName = 'auth';
 
 export const saveSession = async (token, authUser) => {
-	await redis.set(token, JSON.stringify(authUser)); // 1 week
-	await redis.expire(token, SESSION_EXPIRED);
+  await redis.set(token, JSON.stringify(authUser)); // 1 week
+  await redis.expire(token, SESSION_EXPIRED);
 };
 
 export const createSession = async (authUser, cookies) => {
-	// Use node crypto to generate a random token
-	const token = crypto.randomUUID();
-	await saveSession(token, authUser);
-	// const token = Math.random().toString(36).slice(2);
-	cookies.set(cookieName, token, {
-		httponly: true,
-		sameSite: 'lax',
-		path: '/',
-		maxAge: 86400, // 1 day
-		// Secure when in production
+  // Use node crypto to generate a random token
+  const token = crypto.randomUUID();
+  await saveSession(token, authUser);
+  // const token = Math.random().toString(36).slice(2);
+  cookies.set(cookieName, token, {
+    httponly: true,
+    sameSite: 'lax',
+    path: '/',
+    maxAge: 86400, // 1 day
+    // Secure when in production
 
-		secure: import.meta.env.PROD ? true : false
-	});
+    secure: import.meta.env.PROD ? true : false
+  });
 };
 
 export const getToken = (cookies) => {
-	return cookies.get(cookieName);
+  return cookies.get(cookieName);
 };
 
 export const deleteSession = async (cookies) => {
-	// Delete token from redis
-	const token = getToken(cookies);
-	await redis.del(token);
-	// Delete cookie
-	cookies.set(cookieName, '', {
-		httponly: true,
-		sameSite: 'lax',
-		path: '/',
-		maxAge: 0,
-		// Secure when in production
+  // Delete token from redis
+  const token = getToken(cookies);
+  await redis.del(token);
+  // Delete cookie
+  cookies.set(cookieName, '', {
+    httponly: true,
+    sameSite: 'lax',
+    path: '/',
+    maxAge: 0,
+    // Secure when in production
 
-		secure: import.meta.env.PROD ? true : false
-	});
-	throw redirect(303, `${base}/login`);
+    secure: import.meta.env.PROD ? true : false
+  });
+  throw redirect(303, `${base}/login`);
 };
 
 export const getSessionUser = async (cookies) => {
-	const token = getToken(cookies);
-	if (!token) return;
-	const user = await redis.get(token);
-	if (!user) {
-		await deleteSession(cookies);
-		return;
-	}
-	return { token, user: JSON.parse(user) };
+  const token = getToken(cookies);
+  if (!token) return;
+  const user = await redis.get(token);
+  if (!user) {
+    await deleteSession(cookies);
+    return;
+  }
+  return { token, user: JSON.parse(user) };
 };
 
 export const addToSessionStream = async (event, aggregateId, payload) => {
-	const streamData = {
-		streamKeyName: `${STREAM}:sessions`,
-		aggregateId,
-		payload,
-		event: `${event}`,
-		serviceName: SERVICE
-	};
-	await addToEventLog(redis, streamData);
+  const streamData = {
+    streamKeyName: `${STREAM}:sessions`,
+    aggregateId,
+    payload,
+    event: `${event}`,
+    serviceName: SERVICE
+  };
+  await addToEventLog(redis, streamData);
 };
