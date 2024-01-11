87cc9a226d0611d3c47d1391b0473bef6ed436ec
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2024-01-11 09:50:30 -0400

    adde try catch
    Just for starting project prevent 500 error

--- auth-simple/src/routes/app/+layout.server.js
+++ auth-simple/src/routes/app/+layout.server.js
@@ -1,15 +1,19 @@
-import { pbAdmin } from '$lib/server/pb-admin';
-import { serializePOJO } from '$lib/utils';
+import { pbAdmin } from "$lib/server/pb-admin";
+import { serializePOJO } from "$lib/utils";
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ locals }) {
-	const records = await pbAdmin.collection('app_sessions').getList(1, 50, {
-		sort: '-created'
-	});
-	const sessions = serializePOJO(records.items);
+  let sessions = [];
+  try {
+    const records = await pbAdmin.collection("app_sessions").getList(1, 50, {
+      sort: "-created",
+    });
+    sessions = serializePOJO(records.items);
+  } catch (error) { }
 
-	return {
-		user: locals.user,
-		sessions
-	};
+  return {
+    user: locals.user,
+    sessions,
+  };
 }
+
